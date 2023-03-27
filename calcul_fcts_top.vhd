ed19a8ae3f89e67f6bfd8912a11be7bfb261ec20
loic <bigzb@pop-os.localdomain>
2023-03-27 18:01:45 +0200

    vhdl

--- calcul_fcts_top.vhd
+++ calcul_fcts_top.vhd
@@ -4,17 +4,13 @@
 --
 -- File         : calcul_fcts_top.vhd
 --
--- Description  : 
+-- Description  : Description d'un system avec deux entrées na et na ainsi
+--		  que une entrée sel. Lorque sel et à 1: f = na*3-nb+42 et
+--		  f = na-84+2*nb lorsque sel = 0.
 --
--- Author       : L. Fournier
+-- Author       : Brasey Loic et Bastien Pillonel
 -- Date         : 06.02.2023
 -- Version      : 1.0
---
--- Use          :
---
---| Modifications |-------------------------------------------------------------
--- Version   Auteur      Date               Description
--- 1.0       LFR         06.02.2023         Empty version.
 --------------------------------------------------------------------------------
 
 --| Library |-------------------------------------------------------------------
@@ -64,13 +60,12 @@ architecture struct of calcul_fcts_top is
 
 begin
 
-    --| Components instanciation |----------------------------------------------
+    -- affectation des signeaux du premier bloc additionneur
     na_sur_2_s(7 downto 6) <=  (others => na_i(7));
     na_sur_2_s(5 downto 0) <=  na_i(6 downto 1); 
     na_and_sel_s <= na_i when sel_i = '1' else (others => '0');
-    --na_sur_2_s(5 downto 0) => na_i(6 downto 1); 
     
-    
+    -- instanciation de l'additionneur du premier bloc
     add8: addn
     generic map ( N => 8 )
     port map(nbr_a_i   => na_sur_2_s,
@@ -79,26 +74,30 @@ begin
              somme_o   => operande_1_s(8 downto 1)
              -- cout_o 
              );
-    
+
+    -- multiplication par 2 et extension du signe
     operande_1_s(0) <= na_i(0);
     operande_1_s(10 downto 9) <= (others => na_i(na_i'high)); 
     
-    
+    -- assignation des signaux du deuxième bloc (inverser un des deux en fct de sel)
     nb_s <= nb_i(7) & nb_i when sel_i = '0' else not(nb_i(7) & nb_i); 
-    n42_s <= '0' & x"2a" xor not(8 downto 0 => sel_i);
-    --n42_s <= '0' & x"2a" when sel_i = '1' else not( '0' & x"2a");
+    n42_s <= '0' & x"2a" when sel_i = '1' else not( '0' & x"2a");
     
+    -- instanciation du deuxieme additionneur
     add9: addn
     generic map ( N => 9 )
     port map(nbr_a_i   => nb_s,
-             nbr_b_i   => n42_S,
+             nbr_b_i   => n42_s,
              cin_i     => '1',
              somme_o   => rslt_add9_s
              -- cout_o 
              );
+
+    -- multiplication du resulat par 2 en fonction de sel et extension de signe
     operande_2_s(9 downto 0) <= rslt_add9_s(8) & rslt_add9_s when sel_i ='1' else rslt_add9_s & '0';
     operande_2_s(10) <= operande_2_s(9);
-    
+
+    -- instanciation du dernier additionneur    
     add11: addn
     generic map ( N => 11 )
     port map(nbr_a_i   => operande_1_s,
@@ -107,15 +106,9 @@ begin
              somme_o   => f_s
              -- cout_o 
              );
-    
-    
-    
+   
+    -- sortie
     f_o <= f_s;
-    ----------------------------------------------------------------------------
-
-    --| Outputs affectation |---------------------------------------------------
-    
-    ----------------------------------------------------------------------------
 
 end struct;
 --------------------------------------------------------------------------------
